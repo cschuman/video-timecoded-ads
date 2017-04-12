1fbaf4ecf590d99b1d05022456bf53157a21e6dd
Corey Schuman <coreyschuman@coreys-mbp.attlocal.net>
2017-04-11 22:34:40 -0400

    configuration of basic video player complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
 console.log('\'Allo \'Allo!');
+
+var mediaPlayer;
+
+document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
+
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('media-video');
+   mediaPlayer.controls = true;
+
+   mediaPlayer.addEventListener('timeupdate', updateProgressBar, false);
+}
+
+function updateProgressBar() {
+  //  var progressBar = document.getElementById('progress-bar');
+   var percentage = Math.floor((100 / mediaPlayer.duration) *
+                                      mediaPlayer.currentTime);
+  //  progressBar.value = percentage;
+  //  console.log(percentage + '% played');
+   console.log(mediaPlayer.currentTime);
+}
